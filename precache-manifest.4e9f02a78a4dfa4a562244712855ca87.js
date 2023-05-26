0c9bca1dc4371eb5874ef266b33eb1ed84fb5cf3
Code Sandbox <hello@codesandbox.io>
2023-05-26 19:38:55 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4e9f02a78a4dfa4a562244712855ca87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c457aeb7ff5423ca3ef9a3c6bd38737",
+    "url": "https://jtarbard.github.io/csb-1ozk7k/index.html"
+  },
+  {
+    "revision": "8f6402fe908a8776f326",
+    "url": "https://jtarbard.github.io/csb-1ozk7k/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "d88e7f2301b760d18705",
+    "url": "https://jtarbard.github.io/csb-1ozk7k/static/js/2.02f1c83c.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "https://jtarbard.github.io/csb-1ozk7k/static/js/2.02f1c83c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f6402fe908a8776f326",
+    "url": "https://jtarbard.github.io/csb-1ozk7k/static/js/main.773308ab.chunk.js"
+  },
+  {
+    "revision": "d6e8e6408d0e5608a0f1",
+    "url": "https://jtarbard.github.io/csb-1ozk7k/static/js/runtime-main.22e73ae5.js"
+  }
+]);
